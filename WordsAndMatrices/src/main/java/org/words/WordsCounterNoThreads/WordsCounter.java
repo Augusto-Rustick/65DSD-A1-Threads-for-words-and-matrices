0017b7dc4af2a4ebd36ed07a0cb8ebb2479af50f
Augusto-Rustick <a.rustick@outlook.com>
2023-05-04 02:02:35 -0300

    melhoria no algorítmo das palavras

--- WordsAndMatrices/src/main/java/org/words/WordsCounterNoThreads/WordsCounter.java
+++ WordsAndMatrices/src/main/java/org/words/WordsCounterNoThreads/WordsCounter.java
@@ -1,40 +1,42 @@
 package org.words.WordsCounterNoThreads;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordsCounter {
    public static void main(String[] args) {
-      //coleta um arrei com os arquivos de texto do diretorio do dataset
+      Instant startTime = Instant.now();
+      // pergunta ao usuário a palavra desejada
+      String word = "hello"; //hello   coffee   traditional
+      // coleta um array com os arquivos de texto do diretorio do dataset
       String pasta = "src/main/java/org/words/dataset";
       File diretorio = new File(pasta);
       File[] arquivos = diretorio.listFiles();
       //variável de auxilio para o contador
       int countWord = 0;
-      String word = "hello";
+      //
+      Pattern pattern = Pattern.compile("\\b" + word + "\\b");
       //intera para cada arquivo de txt
       for (File arquivo : arquivos) {
-         if (arquivo.isFile() && arquivo.getName().endsWith(".txt")) {
-            try (BufferedReader leitor = new BufferedReader(new FileReader(arquivo))) {
-               String linha;
-               int counterFile = 0;
-               while ((linha = leitor.readLine()) != null) {
-                  String[] palavras = linha.split("\\s+");
-                  for (String palavra : palavras) {
-                     if (palavra.equalsIgnoreCase(word)) {
-                        counterFile++;
-                        countWord++;
-                     }
-                  }
+         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(arquivo), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+               Matcher matcher = pattern.matcher(line);
+               while (matcher.find()) {
+                  countWord++;
                }
-            } catch (IOException e) {
-               System.out.println("Erro ao ler o arquivo " + arquivo.getName() + ": " + e.getMessage());
             }
+         } catch (IOException e) {
+            e.printStackTrace();
          }
       }
       System.out.println("Em todos os arquivos, a palavra '" + word + "' foi encontrada " + countWord + " vezes.");
+      Instant endTime = Instant.now();
+      long duration = Duration.between(startTime, endTime).toMillis();
+      System.out.println("Duração: " + duration + " milissegundos");
    }
 }
